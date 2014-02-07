cda967d8f253328267038d6ad70b836601903701
oott123 <ip.192.168.1.1@qq.com>
2014-02-07 18:26:16 +0800

    将node_modules从版本库中去除;加入favicon.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(flash());
-app.use(express.favicon());
+//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.methodOverride());
 app.use(express.bodyParser());
